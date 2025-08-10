19fcabaac3bdb535f4e0289b62d86721646d7a3b
IHIRINA <2582067027@qq.com>
2025-08-10 22:14:08 +0800

    架子

---
+++ README.md
@@ -0,0 +1,7 @@
+初始化项目：npm init vue@latest
+
+下载element plus：npm install element-plus --save
+
+git操作：        
+git init         
+git add remote origin 。。。       
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" :key="$route.fullPath" />
+    </keep-alive>
+  </RouterView>
+</template>
